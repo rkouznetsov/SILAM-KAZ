8d727a8390af05c3426f1b5569859908a3a16c5a
Rostislav Kouznetsov <rostislav.kouznetsov@fmi.fi>
2022-08-31 20:32:18 +0300

    Switch to v5_8 0.6-deg boundaries

--- get_AQ_BND.sh
+++ get_AQ_BND.sh
@@ -13,20 +13,22 @@ basedate=`date -u -d "$basedate" +%Y%m%d`
 set -u 
 ncks=ncks
 
-BND_PATH=${BOUNDARY_DIR}571
+BND_PATH=${BOUNDARY_DIR}58
 
 targetdir=$BND_PATH/`date -u -d "$basedate" +%Y%m%d00`
 mkdir -p $targetdir
 cd $targetdir
+
+echo Getting boundaries to `pwd`
 
 
 
 #echo $SHELL
 #exit 1
 
-runpref="silam_glob_v5_7_1_RUN_"
-urlbaseNCSS="https://silam.fmi.fi/thredds/ncss/silam_glob_v5_7_1/runs/$runpref"
-urlbase="https://silam.fmi.fi/thredds/dodsC/silam_glob_v5_7_1/runs/$runpref"
+runpref="silam_glob06_v5_8_RUN_"
+urlbaseNCSS="https://silam.fmi.fi/thredds/ncss/silam_glob06_v5_8/runs/$runpref"
+urlbase="https://silam.fmi.fi/thredds/dodsC/silam_glob06_v5_8/runs/$runpref"
 
 #List form global v5_7_1 that is pesent in IND
 species="AACD_gas ALD2_gas ALDX_gas AVB0_gas AVB0_m_50 AVB1e0_gas AVB1e0_m_50 AVB1e1_gas AVB1e1_m_50 AVB1e2_gas AVB1e2_m_50 AVB1e3_gas AVB1e3_m_50 AVB1e4_gas AVB1e5_gas AVB1e6_gas BENZENE_gas BVB0_gas BVB0_m_50 BVB1e0_gas BVB1e0_m_50 BVB1e1_gas BVB1e1_m_50 BVB1e2_gas BVB1e2_m_50 BVB1e3_gas BVB1e3_m_50 C2O3_gas C5H8_2_gas C5H8_gas CH3Cl_gas CO_gas CRES_gas CRO_gas CXO3_gas EC_m_50 ETHA_gas ETH_gas ETOH_gas FACD_gas H2O2_gas HCHO_gas HCO3_gas HNO3_gas HO2_gas HONO_gas IOLE_gas ISPD_gas MEO2_gas MEOH_gas MEPX_gas MGLY_gas N2O5_gas NH3_gas NH415SO4_m_20 NH415SO4_m_70 NH4NO3_m_70 NO2_gas NO3_c_m3_0 NO3_gas NO_gas NTR_gas O1D_gas O3_gas OH_gas OLE5_gas OPEN_gas O_gas PACD_gas PANX_gas PAN_gas PAR5_gas PM_m6_0 PNA_gas ROOH_gas ROR5_gas SESQ_gas SO2_gas SO4_m_20 SO4_m_70 TO2_gas TOL_gas XO2N_gas XO2_gas XYL_gas dust_m1_5 dust_m20 dust_m6_0 dust_m_30 sslt_m20 sslt_m3_0 sslt_m9_0 sslt_m_05 sslt_m_50"
@@ -36,7 +38,7 @@ species="AACD_gas ALD2_gas ALDX_gas AVB0_gas AVB0_m_50 AVB1e0_gas AVB1e0_m_50 AV
 varlist="a,b,da,db,a_half,b_half,O3_column,NO2_column,air_dens"
 
 for sp in $species; do
-    varlist="$varlist,cnc_${sp}"
+    varlist="$varlist,vmr_${sp}"
 done
 
 echo $varlist
@@ -62,7 +64,7 @@ for try  in `seq 0 10`; do
    missfiles=""
    for hr in `seq 48 3 168` ; do
    #for hr in `seq 48 52` ; do
-        outf=`date -u -d"$basedate + $hr hours" +"SILAM571${suitename}${run}_%Y%m%d%H.nc"`
+        outf=`date -u -d"$basedate + $hr hours" +"SILAM58${suitename}${run}_%Y%m%d%H.nc"`
 
         [ -f $outf ] && continue
         step=`expr $hr - 1`
@@ -76,7 +78,7 @@ for try  in `seq 0 10`; do
         #
         # For some reason thredds does not supply _CoordinateModelRunDate anymore
         attcmd="-a _CoordinateModelRunDate,global,c,c,$run -a history,global,d,,, -a history_of_appended_files,global,d,,, -a _ChunkSizes,,d,,,"
-        compresscmd="-h --mk_rec_dmn time -4 -L5  --cnk_dmn hybrid,1 --cnk_map=rd1 --ppc cnc_.*=2"
+        compresscmd="-h --mk_rec_dmn time -4 -L5  --cnk_dmn hybrid,1 --cnk_map=rd1 --ppc vmr_.*=2"
 
         #   get -> fix attribures -> compress
         (ncks -O $bbox -d time,$step -v ${varlist} "$URL" ${outf}.tmp && ncatted -h $attcmd ${outf}.tmp && $ncks $compresscmd ${outf}.tmp $outf && rm ${outf}.tmp && echo  $outf done!) & 
